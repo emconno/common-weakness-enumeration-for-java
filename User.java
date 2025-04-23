cb9f8deab352ab0d05d6285a0780497149629077
emconno <emconno22@gmail.com>
2025-04-23 13:26:27 -0500

    add user class

--- User.java
+++ User.java
@@ -1,3 +1,29 @@
 public class User {
+    public String name;
+    public int age;
+    public Listing[] listings;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.listings = new Listing[10];
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
     
 }
